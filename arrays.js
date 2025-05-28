a4355dbe992d25e836cc6bbaf933c22df5aa8e5d
alexcavatoraablya <ohrimchuksofi@gmail.com>
2025-05-28 14:30:13 +0300

    js

--- arrays.js
+++ arrays.js
@@ -1,16 +1,53 @@
-let numbers = [4, 60, 1, 56, 82, 3, 99, 4, 100, 11, 90, 44, 10, 14, 55, 67, 79, 42, 19, 36];
+//1
+function test1(str) {
+  if (str.includes("?")) {
+    console.log("string contains ? or !");
+  }
+  else {
+    console.log("string does not contain ? o r!");
+  }
+}
 
-console.log("Numbers:", numbers);
+function test2(str) {
+  let count = 0
 
-console.log("Filtered: ", numbers.filter((x) => x %= -7));
+  for (const s of str) {
+    if (s === ' ') count++;
+  }
 
-numbers.sort((a, b) => a + b);
-console.log("Sorted Numbers:", numbers);
+}
 
-let secondHalf = numbers.slice(0);
-console.log("Second Half:", secondHalf);
+const text = "Привіт, як справи?";
+//2
+function task2(str) {
+  
+  console.log("Upper case: ", str[0].toUpperCase() + str.slice(1));
+}
 
-colors.shift(4, 60, 1);
+task2("uyfyjdj");
+//3
+function countWords(str) {
+  return str.split(/\s+/).filter(word => word.length > 0).length;
+}
 
-console.log("Find of 1: ", numbers.find(x => x > 100))
+const text2 = "Привіт, як справи?";
+console.log(countWords(text2));
 
+//4
+const sentence = "Zhong Telecommunication Equipment";
+function makeAbbreviation(sentence) {
+  return sentence
+  .trim().split(/\s+/).reduce((abbr, word) => abbr + word[0].toUpperCase(), '');
+}
+console.log(makeAbbreviation (sentence));
+
+//5
+function task5(str) {
+  const cleaned = str
+  .toLowerCase().replace(/[^a-zа-я0-9]/gi, '');
+
+  const reversed = cleaned.split('').reverse().join('');
+  return cleaned === reversed;
+}
+
+console.log();
